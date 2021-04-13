ad2a57f4d4c127cb781b8f6122907fcebaa3bd34
Jeremy Rand <jeremy@rand-family.com>
2021-04-13 15:10:02 -0400

    Initial Commit

---
+++ md2teach/main.c
@@ -0,0 +1,18 @@
+/*
+ * main.c
+ * md2teach
+ *
+ * Created by Jeremy Rand on 2021-04-13.
+ * Copyright (c) 2021 ___ORGANIZATIONNAME___. All rights reserved.
+ *
+ */
+
+
+#include <stdio.h>
+
+
+int main(void)
+{
+    printf("Hello, world!\n");
+    return 0;
+}
