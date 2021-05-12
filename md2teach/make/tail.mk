9e8e470ed999d00c037ef8ce17b5654d471919c8
Jeremy Rand <jeremy@rand-family.com>
2021-05-11 23:35:26 -0400

    Using the latest version of GoldenGate, the teachRez script workaround is no longer required.  The md2teach exec works as well now under GoldenGate as it does under GNO/ORCA (which hopefully means it works well, TBD).

--- md2teach/make/tail.mk
+++ md2teach/make/tail.mk
@@ -196,8 +196,7 @@ executeGUI: all
 	make/launchEmulator "$(DISKIMAGE)" "$(DESTBOOTIMAGE)"
 
 executeShell: all
-	$(ORCA) --mem $(TARGETDIR)/$(PGM) -d -r test.md $(TARGETDIR)/outfile.txt
-	make/teachRez $(TARGETDIR)/outfile.txt
+	$(ORCA) --mem $(TARGETDIR)/$(PGM) -d test.md $(TARGETDIR)/outfile.txt
 	make/createDiskImage "$(DISKIMAGE)" $(DESTBOOTIMAGE) "$(TARGETDIR)/$(PGM)" "$(TARGETDIR)/outfile.txt" test.md
 	make/launchEmulator "$(DISKIMAGE)" "$(DESTBOOTIMAGE)"
 
